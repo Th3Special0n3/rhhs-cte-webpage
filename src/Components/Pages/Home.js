b8e2e2d45f7c60630aa3a59de98ebd30c912a5cb
Th3Special0n3 <jaidenjoseph96@gmail.com>
2022-07-19 15:23:11 +0000

    Updated RHHS Page

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -8,13 +8,15 @@ class Home extends Component {
       <div>
         <Splash/>
         <h2> Career and Technical Education programs are made up of elective courses that teach students about different careers. Richmond Hill High School offers several different CTE pathways including: </h2>
-        <ul class = "list">
+        <ul className = "list">
             <li>Hospitalisty and Tourism</li>
             <li>Software Engineering</li>
             <li>Health Sciences</li>
             <li>IT and Robotics</li>
             <li>Art and Design</li>
             <li>Criminology and Forensics</li>
+            <li>Freshman Academy</li>
+            <li>International Academy</li>
         </ul>
         <h3>Explore each of our pathways above to learn about what we do at Richmond Hill!</h3>
       </div>
