686968a23ce6cfb36e16657083a281dbb5c377ab
Th3Special0n3 <jaidenjoseph96@gmail.com>
2022-07-26 15:46:15 +0000

    rhhs-cte-webpage

---
+++ src/Components/Pages/soft-skills.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function SoftSkills (){
+    return(
+        <ul className = "soft-skills">
+            <li>Hospitalisty and Tourism</li>
+            <li>Software Engineering</li>
+            <li>Health Sciences</li>
+            <li>IT and Robotics</li>
+            <li>Art and Design</li>
+            <li>Criminology and Forensics</li>
+            <li>Freshman Academy</li>
+            <li>International Academy</li>
+        </ul>
+    )
+}
+export default SoftSkills
---
+++ src/Components/Pages/technical-skills.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function TechnicalSkills (){
+    return(
+        <ul className = "technical-skills">
+            <li>Hospitalisty and Tourism</li>
+            <li>Software Engineering</li>
+            <li>Health Sciences</li>
+            <li>IT and Robotics</li>
+            <li>Art and Design</li>
+            <li>Criminology and Forensics</li>
+            <li>Freshman Academy</li>
+            <li>International Academy</li>
+        </ul>
+    )
+}
+export default TechnicalSkills
